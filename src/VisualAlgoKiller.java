9ee0c6bcbdb4a27a14d14c2524bca3a7e4a9fdaa
AHaliq <liqo12@hotmail.com>
2015-08-15 22:16:09 +0800

    updated styling

--- src/VisualAlgoKiller.java
+++ src/VisualAlgoKiller.java
@@ -7,49 +7,56 @@ public class VisualAlgoKiller {
 
   public static void main(String[] args) {
     P.nl();
-    P.l("===========================");
-    P.l("Welcome to VisualAlgoKiller");
-    P.l("===========================");
-    P.l("===                     ===");
-    P.l("=       by:FSociety       =");
+    // document style length 30 chars
+    P.l("==============================");
+    P.l(" Welcome to VisualAlgoKiller! ");
+    P.l("==============================");
+    P.l("====                      ====");
+    P.l("=        by: FSociety        =");
     P.nl();
     // greeter
 
-    int choice = P.getChoice("Select your topic type",
-    "Linked List", "Recursion", "Sorting", "Hash Table");
+    int choice = P.getChoice(
+        "  Select your topic type  ",
+          "Linked List",
+          "Recursion",
+          "Sorting",
+          "Hash Table");
     int choice2 = 1;
     // request topic
 
-    String requestMsg = "Select your question type";
+    String requestMsg =
+        "Select your question type ";
     switch(choice) {
       case 1:
       choice2 = P.getChoice(requestMsg,
-      "Queue", "Singly Sum", "Stack");
+          "Queue",
+          "Singly Sum",
+          "Stack");
       break;
       case 2:
       choice2 = P.getChoice(requestMsg,
-      "TestFunction 1 param ", "TestFunction 2 param", "doSomethingCool");
+          "TestFunction 1 param ",
+          "TestFunction 2 param",
+          "doSomethingCool");
       break;
       case 3:
       choice2 = P.getChoice(requestMsg,
-      "How many X; this version sort", "Sequence", "Quicksort");
+          "How many X; this __ sort",
+          "Sequence",
+          "Quicksort");
       break;
       case 4:
       choice2 = P.getChoice(requestMsg,
-      "Suppose... Yes/No", "Suppose... table sequence", "Consider... m=12");
+          "Suppose... Yes/No",
+          "Suppose... table sequence",
+          "Consider... m=12");
       break;
     }
     // request question
 
-    P.nl();
-    P.l("===========================");
-    solver[choice - 1][choice2 - 1].solve();
+    P.l("==============================");
+    SolverWrapper.solver[choice - 1][choice2 - 1].solve();
     // execute solver based on user's choice
   }
-
-  // View ---------------------------------------------------------------------
-
-
-
-  // Model + Controller -------------------------------------------------------
 }
