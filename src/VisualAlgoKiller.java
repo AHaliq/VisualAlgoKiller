ec3b12148d9fc6a2b90092e1e62a1e93143031cc
AHaliq <liqo12@hotmail.com>
2015-08-17 16:30:44 +0800

    make program repeat till user wishes to terminate

--- src/VisualAlgoKiller.java
+++ src/VisualAlgoKiller.java
@@ -16,47 +16,56 @@ public class VisualAlgoKiller {
     P.nl();
     // greeter
 
-    int choice = P.getChoice(
-        "  Select your topic type  ",
-          "Linked List",
-          "Recursion",
-          "Sorting",
-          "Hash Table");
-    int choice2 = 1;
-    // request topic
+    boolean repeat = false;
+    do {
 
-    String requestMsg =
-        "Select your question type ";
-    switch(choice) {
-      case 1:
-      choice2 = P.getChoice(requestMsg,
-          "Queue",
-          "Singly Sum",
-          "Stack");
-      break;
-      case 2:
-      choice2 = P.getChoice(requestMsg,
-          "TestFunction 1 param ",
-          "TestFunction 2 param",
-          "doSomethingCool");
-      break;
-      case 3:
-      choice2 = P.getChoice(requestMsg,
-          "How many X; this __ sort",
-          "Sequence",
-          "Quicksort");
-      break;
-      case 4:
-      choice2 = P.getChoice(requestMsg,
-          "Suppose... Yes/No",
-          "Suppose... table sequence",
-          "Consider... m=12");
-      break;
-    }
-    // request question
+      int choice = P.getChoice(
+          "  Select your topic type  ",
+            "Linked List",
+            "Recursion",
+            "Sorting",
+            "Hash Table");
+      int choice2 = 1;
+      // request topic
 
-    P.l("==============================");
-    SolverWrapper.solver[choice - 1][choice2 - 1].solve();
-    // execute solver based on user's choice
+      String requestMsg =
+          "Select your question type ";
+      switch(choice) {
+        case 1:
+        choice2 = P.getChoice(requestMsg,
+            "Queue",
+            "Singly Sum",
+            "Stack");
+        break;
+        case 2:
+        choice2 = P.getChoice(requestMsg,
+            "TestFunction 1 param ",
+            "TestFunction 2 param",
+            "doSomethingCool");
+        break;
+        case 3:
+        choice2 = P.getChoice(requestMsg,
+            "How many X; this __ sort",
+            "Sequence",
+            "Quicksort");
+        break;
+        case 4:
+        choice2 = P.getChoice(requestMsg,
+            "Suppose... Yes/No",
+            "Suppose... table sequence",
+            "Consider... m=12");
+        break;
+      }
+      // request question
+
+      P.l("==============================");
+      SolverWrapper.solver[choice - 1][choice2 - 1].solve();
+      // execute solver based on user's choice
+
+      P.bl();
+      P.l("Solve another question? [y/n]");
+      repeat == P.getStrLine().equalsIgnoreCase("y");
+      P.l("==============================");
+    }while(repeat)
   }
 }
